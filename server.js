7d867b9e954ed3b00f72098e9156e77f3b7d891d
Adriel Oliveira <adritis.pessoal@gmail.com>
2016-02-04 10:53:25 -0200

    up admin  mongo

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use('/',express.static('public/html'));
+app.use('/assets',express.static('public'));
 
 /*
  *  AUTO LOAD MODELS , CONTROLLERS AND ROUTES INDO APPLICATION INSTANCE
